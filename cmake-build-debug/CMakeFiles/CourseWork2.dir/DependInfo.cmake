727bf2b68c5b6033ea8230d570c0a99889c95d7f
farismufti <faris.mufti@outlook.com>
2020-03-11 09:25:47 +0000

    First commit

---
+++ cmake-build-debug/CMakeFiles/CourseWork2.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/faris/Desktop/University/Intro to Artificial Intenlligence/CourseWork2/Example_main.c" "C:/Users/faris/Desktop/University/Intro to Artificial Intenlligence/CourseWork2/cmake-build-debug/CMakeFiles/CourseWork2.dir/Example_main.c.obj"
+  "C:/Users/faris/Desktop/University/Intro to Artificial Intenlligence/CourseWork2/SolutionListOperations.c" "C:/Users/faris/Desktop/University/Intro to Artificial Intenlligence/CourseWork2/cmake-build-debug/CMakeFiles/CourseWork2.dir/SolutionListOperations.c.obj"
+  "C:/Users/faris/Desktop/University/Intro to Artificial Intenlligence/CourseWork2/TrainAndTest.c" "C:/Users/faris/Desktop/University/Intro to Artificial Intenlligence/CourseWork2/cmake-build-debug/CMakeFiles/CourseWork2.dir/TrainAndTest.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CourseWork2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CourseWork2.dir/Example_main.c.obj"
+  "CMakeFiles/CourseWork2.dir/SolutionListOperations.c.obj"
+  "CMakeFiles/CourseWork2.dir/TrainAndTest.c.obj"
+  "CourseWork2.exe"
+  "CourseWork2.exe.manifest"
+  "CourseWork2.pdb"
+  "libCourseWork2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CourseWork2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
