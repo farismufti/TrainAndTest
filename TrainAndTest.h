9c66a1661b90d97b5af4ecef5ee973a0c3ed3ae9
farismufti <faris.mufti@outlook.com>
2020-03-25 16:54:40 +0000

    Fixed typos in comments and removed some white space

--- TrainAndTest.h
+++ TrainAndTest.h
@@ -17,7 +17,7 @@
 
 
 /**
- @file TrainandTest.h
+ @file TrainAndTest.h
  @brief one useful type of structure and various specific functions for RuleInduction  Problem
  */
 
@@ -26,7 +26,7 @@
 /*here are some constant values that we will refer to
  * the beauty of using defines here is that now if I ever want to change them, I only need do so in one place
  */
-// it would be cleaner to make these dynamic but for safety resons we can;t have student code dynamically allocating memory
+// it would be cleaner to make these dynamic but for safety reasons we can't have student code dynamically allocating memory
 #define NUM_FEATURES 6
 #define NUM_SAMPLES 150
 #define NUM_TRAINING_SAMPLES 100
@@ -46,7 +46,7 @@
 
 // some rule specific declarations
 #define NUM_OPERATORS 3
-enum operator {lessThan =0, equals = 1, greaterThan = 2};
+enum operator {lessThan = 0, equals = 1, greaterThan = 2};
     
 ///  struct to hold a simple (axis-perpendicular) rule  of form IF feature[X] > threshold THEN class = Y
 typedef struct {
@@ -63,11 +63,11 @@ typedef struct {
 
 
     
-    /// function that calls a machibe learning algorithm and learns a classifier
-    /// returns 1 if successful,  0 if some error condtion occurs
+    /// function that calls a machine learning algorithm and learns a classifier
+    /// returns 1 if successful,  0 if some error condition occurs
     /// @param trainingSamples : 2array -one row per sample, one column per feature
     /// @param trainingLabels : 1D array - one row per sample, of class labels
-    /// @param numSamples : int - the number of sanp,es (rows)
+    /// @param numSamples : int - the number of samples (rows)
     /// @param numFeatures :int the number of features (columns)
 int  train( double **trainingSamples, int *trainingLabels, int numSamples, int numFeatures);
     
@@ -84,35 +84,35 @@ void StoreData(double **trainingSamples, int *trainingLabels, int numSamples, in
     
     /// @brief this is  the part that you have to write
     ///     Your code should make use of the structures and operators provided to create a ruleset
-    ///         Starting with an empty working cndidate, at each iteration it shoul the single rule that most increases the score
-    ///         to test a candidage soltion copy it into th     you should call the function ScoreWorkingCandidateOnTrainingSet()    to test a candidate soltion
+    ///         Starting with an empty working candidate, at each iteration it shoul the single rule that most increases the score
+    ///         to test a candidate solution copy it into th     you should call the function ScoreWorkingCandidateOnTrainingSet() to test a candidate solution
 
     void GreedyConstructiveSearch(void);
 
     
     /// Simple Boolean teast of whether the working candidate
-    ///  (i) classifies all of the trainig set example correctly  OR has no room left to add furthe rules
+    ///  (i) classifies all of the training set example correctly OR has no room left to add further rules
 bool GoalFound(void);
     
-    /// @brief Function that calculates amnd s tortes the scopre for the working candidate
+    /// @brief Function that calculates and stores the score for the working candidate
 ///     Function that interprets the working candidate as a ruleset, and calculates its score,
-///     No return values as it is stored in workingcandidate.score
-///             Note that score is zero if any of the treaiing set are classified incorrectly,  otherwise it is the number of correctly classified samples in the traiing sset
-///   Pre-conditions:  (i) training set has been processed and stored; (ii) workingcandidate contains some rules i.e .size >0
+///     No return values as it is stored in workingCandidate.score
+///             Note that score is zero if any of the training set are classified incorrectly,  otherwise it is the number of correctly classified samples in the training set
+///   Pre-conditions:  (i) training set has been processed and stored; (ii) workingCandidate contains some rules i.e .size >0
 /// Post-conditions: workingCandidate.score is updated.
 void ScoreWorkingCandidateOnTrainingSet(void);
     
     /// @Brief: function to predict the class of an item by applying a simple rule
     /// @param thisRule rule to be applied
     /// @param sample sample ot apply rule to
-    /// @param numFeatures number of featues in sample
+    /// @param numFeatures number of features in sample
 /// Return is  class predicted or NO_PREDICTION if sample does not match rule condition
 int PredictClassFromRule(rule thisRule, double * sample, int numFeatures );
 
 
     
-    /// @Brief Function to print out the ruleset represented within a canidate solution
-    /// preconditino is the existience of a variable called workiongCandidate that holds the learned rule set
+    /// @Brief Function to print out the ruleset represented within a candidate solution
+    /// precondition is the existence of a variable called workingCandidate that holds the learned rule set
 void printWorkingCandidate (void);
 
 
@@ -129,9 +129,8 @@ void printWorkingCandidate (void);
 void ExtendWorkingCandidateByAddingRule(rule newRule);
 
 
-    
-    
-    /// function that initialsies the local storage ready to hold the trainig data
+
+    /// function that initialises the local storage ready to hold the training data
     /// @param numSamples number of examples (rows)
     /// @param numFeatures number of features (columns)
 void prepareTrainingDataArrays(int numSamples, int numFeatures);
